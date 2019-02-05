6e64b23ce5eb4e5756204c117feaa4991caf3ca1
AnzarulHaque <anzarul.haque@smail.rasmussen.edu>
2019-02-04 23:06:04 -0600

    Book Inventory UWP Application

---
+++ App2/App2/BookDelete.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.Storage;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BookDelete : Page
+    {
+        public BookDelete()
+        {
+            this.InitializeComponent();
+        }
+
+        IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+        MobileServiceCollection<TodoItem, TodoItem> items;
+
+        public class TodoItem
+        {
+            public string Id { get; set; }
+            public string BookTitle { get; set; }
+            public bool Complete { get; set; }
+            public string Author { get; set; }
+            public string Quantity { get; set; }
+            public string LastUpdated { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private async Task FetchTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await todoTable
+                    .Where(TodoItem => TodoItem.Complete == false)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+                this.btnFetchBooks.IsEnabled = true;
+            }
+        }
+
+        private async void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            await UpdateCheckedTodoItem(item);
+        }
+
+        private async Task UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await todoTable.UpdateAsync(item);
+            items.Remove(item);
+            ListItems.Focus(Windows.UI.Xaml.FocusState.Unfocused);
+
+            //await SyncAsync(); // offline sync
+        }
+
+        async private void btnViewBookInventory(object sender, RoutedEventArgs e)
+        {
+            await FetchTodoItems();
+        }
+
+
+    }
+}
